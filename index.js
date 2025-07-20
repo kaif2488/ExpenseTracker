2f5b86ec85498c633306d5b9469b29b8a5453d63
Kaif Siddique <kaifsiddique2488@gmail.com>
2025-07-20 23:59:58 +0530

    day3

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const cors = require('cors');
 
 const db = require('./db');  ///import from db.js 
-const app = express();
 
+const transactionRoutes = require('./routes/transactionRoutes');
+
+const app = express();
 //MidleWare
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>
-{
+app.use('/api', transactionRoutes);
+
+app.get('/', (req, res) => {
     res.send('🚀 Expense Tracker API is running!');
 });
 
 const PORT = 5000;
-app.listen(PORT,()=> {
-    console.log('✅ Server started on http://localhost:${PORT}');
+app.listen(PORT, () => {
+    console.log(`✅ Server started on http://localhost:${PORT}`);
 
 });
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const { addTransaction } = require('../Controllers/transactionController');
+
+router.post('/Transaction', addTransaction);
+module.exports = router;
