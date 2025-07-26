1aea5b7387809d3bed4b1d4f8bdb9fd923468f4d
Kaif Siddique <kaifsiddique2488@gmail.com>
2025-07-26 23:53:09 +0530

    Get data feature

--- Controllers/transactionController.js
+++ Controllers/transactionController.js
@@ -1,12 +1,14 @@
 const db = require('../db');
 
+////Add Transaction
+
 const addTransaction = (req, res) => {
   const { description, amount, type } = req.body;
 
   if (!description || !amount || !type) {
     return res.status(400).json({ error: 'Please fill all fields' });
   }
-  const sql = 'INSERT INTO transactions (description, amount, type) VALUES (?, ?, ?)';
+  const sql = 'INSERT INTO transaction (description, amount, type) VALUES (?, ?, ?)';
   const values = [description, amount, type];
 
   db.query(sql, values, (err, result) => {
@@ -17,4 +19,19 @@ const addTransaction = (req, res) => {
     res.status(201).json({ message: 'Transaction added', id: result.insertId });
   });
 };
+
+////Get Transaction
+
+const getTransaction =(req, res) =>{
+  const query = 'SELECT * FROM transaction';
+  db.query(query,(err,result) =>{
+    if(err){
+      console.log('❌ DB Error fetching transactions:', err);
+    return res.status(500).json({error:'Error fetching'});
+    }
+    res.status(200).json(result);
+  });
+};
+
 module.exports = { addTransaction };
+module.exports = {getTransaction};
